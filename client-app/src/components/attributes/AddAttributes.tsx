20e500b6731fff69b773c5f098aad1908fdb216c
Nelson Rivera <rivera.nelson.r@gmail.com>
2023-04-13 22:45:56 -0400

    set attributes and dispatch changes to the parent

--- client-app/src/components/attributes/AddAttributes.tsx
+++ client-app/src/components/attributes/AddAttributes.tsx
@@ -1,75 +1,78 @@
-import { HTMLInputTypeAttribute, ReactElement, useEffect, useState } from "react"
+import { HTMLInputTypeAttribute, ReactElement, useEffect, useState } from "react";
 
 
-const AddAttributes = ({attributes, propertyID}:{attributes:any [], propertyID:number}) => {
+const AddAttributes = ({ attributes, propertyID, setAttributes }: { attributes: any[], propertyID: number, setAttributes: any; }) => {
 
-    const attributesToSet:any [] = new Array(attributes.length).fill({})
+    const attributesToSet: any[] = new Array(attributes.length).fill({});
     // estado que depende de attributes 
-    const [checkedState, setCheckedState] = useState( new Array(attributes.length).fill(false) );
-    const [quantity, setQuantity] = useState( new Array(attributes.length).fill(0) );
+    const [checkedState, setCheckedState] = useState(new Array(attributes.length).fill(false));
+    const [quantity, setQuantity] = useState(new Array(attributes.length).fill(0));
 
     useEffect(() => {
         setCheckedState(new Array(attributes.length).fill(false));
-        setQuantity(new Array(attributes.length).fill(0))
-    }, [attributes])
-    
+        setQuantity(new Array(attributes.length).fill(0));
+    }, [attributes]);
+
     // controlador de eventos de los checkbox
-    const handleOnChange = (position:number) => {
-        
+    const handleOnChange = (position: number) => {
+
         const updatedCheckedState = checkedState.map((item, index) =>
             index === position ? !item : item
         );
 
-        setCheckedState(updatedCheckedState); 
-    }
-    
+        setCheckedState(updatedCheckedState);
+    };
 
     // [{}:{}:{}]
     // checkedState = [false, false, false]
-    const handleAttributes = async (e: any, id:number) => {
-        const {name, value} = e.target
-        await handleOnChange(id)
-        checkedState.map((attr, index) => {
-            console.log('comparacion', index === id)
+    const handleAttributes = (e: any, id: number) => {
+        handleOnChange(id);
+    };
 
-            if(attr === true && index === id) 
-            {
-                (attributesToSet[index] = {
-                    id:0, 
-                    propertyId: propertyID, 
-                    attributeId: value, 
-                    quantity: quantity[index]
-                })
-            }
-        })
-        console.log('array to set',attributesToSet)
+    const handleQuantity = (e: any, id: number) => {
+        const { value } = e.target;
+        const _quantity = [...quantity];
+        _quantity[id] = value === "" ? 0 : parseInt(value);
+        setQuantity(_quantity);
+    };
 
-    }
+    useEffect(() => {
+        const _attributes = checkedState.map((value, index) => {
+            return { ...attributes[index], status: value, quantity: quantity[index] };
+        });
 
-    return(
+        setAttributes(_attributes);
+    }, [checkedState, quantity, setAttributes, attributes]);
+
+    return (
         <div className="form-group mb-3">
-                            {attributes.map((attribute, index) => {
-                                return(
-                                    <div key={index} className="form-check form-check-inline">
-                                        <input className="form-check-input" type="checkbox" 
-                                        value={attribute.id} name="attributeId" id={`checkbox-${attribute.id}`} 
-                                        onChange={(event) => handleAttributes(event, index)} checked={checkedState[index]} />
-                                        <label className="form-check-label" 
-                                        htmlFor={`checkbox-${attribute.id}`}>
-                                            {attribute.description}
-                                        </label>
+            {attributes.map((attribute, index) => {
+                return (
+                    <div key={index} className="form-check form-check-inline">
+                        <input className="form-check-input" type="checkbox"
+                            value={attribute.id} name="attributeId" id={`checkbox-${attribute.id}`}
+                            onChange={(event) => handleAttributes(event, index)} checked={checkedState[index]} />
+                        <label className="form-check-label"
+                            htmlFor={`checkbox-${attribute.id}`}>
+                            {attribute.description}
+                        </label>
 
-                                        <label className="form-label ms-3 me-1" 
-                                        htmlFor={`inputQuantity-${attribute.id}`}>
-                                            Cantidad
-                                        </label>
-                                        <input type="number" className={`form-control ${checkedState[index] ? "":"readonly`"}`}  
-                                        name="quantity" id={`inputQuantity-${attribute.id}`} readOnly={!checkedState[index]}/>
-                                    </div>
-                                )
-                            })}
-                        </div>
-    )
-}
+                        <label className="form-label ms-3 me-1"
+                            htmlFor={`inputQuantity-${attribute.id}`}>
+                            Cantidad
+                        </label>
+                        <input
+                            type="number"
+                            className={`form-control ${checkedState[index] ? "" : "readonly`"}`}
+                            name="quantity"
+                            id={`inputQuantity-${attribute.id}`}
+                            onChange={(event) => handleQuantity(event, index)}
+                            readOnly={!checkedState[index]} />
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
 
 export default AddAttributes;
--- client-app/src/components/properties/AddProperty.tsx
+++ client-app/src/components/properties/AddProperty.tsx
@@ -1,50 +1,53 @@
 import { format } from "date-fns";
-import {  useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AddAttributes from "../attributes/AddAttributes";
 
-const AddProperty = ({usuarioInfo}:{usuarioInfo:{id: number,name: string,
-    email: string, phone: string,}}) => {
+const AddProperty = ({ usuarioInfo }: {
+    usuarioInfo: {
+        id: number, name: string,
+        email: string, phone: string,
+    };
+}) => {
 
     let propertyID: number = 0;
     let sizeForAttributeArray = 0;
-    
-    const formData = new FormData()
+
+    const formData = new FormData();
     const navigate = useNavigate();
 
-    const [images, setImages] = useState([] as any [])
-    const [attributes, setAttributes] = useState([] as any [])
-    const [attributesToPost, setAttributesToPost] = useState([] as any [])
-    const [propertyAttributes, setPropertyAttributes] = useState([] as any [])
-    const [uploadError, setUploadError] = useState(false)
-   
-    
-    const [property, setProperty] = useState({
-            id: 0,
-            createdAt: new Date(),
-            sellerId: usuarioInfo.id,
-            titulo: '',
-            price: 0.00,
-            areaTotal: 0.00,
-        })
+    const [images, setImages] = useState([] as any[]);
+    const [attributes, setAttributes] = useState([] as any[]);
+    const [attributesToPost, setAttributesToPost] = useState([] as any[]);
+    const [propertyAttributes, setPropertyAttributes] = useState([] as any[]);
+    const [uploadError, setUploadError] = useState(false);
 
-        useEffect(() => {
-            fetch('https://localhost:7272/api/Attribute')
+
+    const [property, setProperty] = useState({
+        id: 0,
+        createdAt: new Date(),
+        sellerId: usuarioInfo.id,
+        titulo: '',
+        price: 0.00,
+        areaTotal: 0.00,
+    });
+
+    useEffect(() => {
+        fetch('https://localhost:7272/api/Attribute')
             .then((response) => {
                 return response.json();
             })
             .then((data) => {
-                setAttributes(data)
-            })
-        }, [])
+                setAttributes(data);
+            });
+    }, []);
 
-   
+
     const handleImage = (event: any) => {
-        if(checkFileLimits(Array.from(event.target.files).length))
-        {
-            setImages([...event.target.files])        
+        if (checkFileLimits(Array.from(event.target.files).length)) {
+            setImages([...event.target.files]);
         }
-    }
+    };
 
     const checkFileLimits = (quantity: number) => {
         const MAX_QUANTITY = 15;
@@ -55,15 +58,20 @@ const AddProperty = ({usuarioInfo}:{usuarioInfo:{id: number,name: string,
         } else {
             return true;
         }
-    }
+    };
 
     const handleChange = (e: any) => {
         const { name, value } = e.target;
-        setProperty({...property, [name]:value})
-    }
+        setProperty({ ...property, [name]: value });
+    };
 
-   
-    const handleSubmit = async(e: any) => {
+    const handleSetAttributes = (attributes: any) => {
+        // update attributes
+        console.log(attributes);
+    };
+
+
+    const handleSubmit = async (e: any) => {
         e.preventDefault();
         const propertyValue = {
             createdAt: property.createdAt,
@@ -71,128 +79,131 @@ const AddProperty = ({usuarioInfo}:{usuarioInfo:{id: number,name: string,
             titulo: property.titulo,
             price: property.price,
             areaTotal: property.areaTotal
-        }
-        
+        };
+
         //Guardar publicacion y devolver su id
         try {
-            
+
             await fetch('https://localhost:7272/api/Property', {
-            method: 'POST',
-            headers:  {'Content-type':'application/json; charset=UTF-8'},
-            body: JSON.stringify(propertyValue)
-                
-            }).then((response) => response.json())
-            .then((data) => {
-                propertyID = data;
-            });  
-           
-        } catch (error) {
-            console.log("error in post property", error)
-        }
-
-        
-        
-        try {
-            
-            await fetch('https://localhost:7272/api/PropertiesAttribute', {
-            method: 'POST',
-            headers:  {'Content-type':'application/json; charset=UTF-8'},
-            body: JSON.stringify(propertyValue)
-                
-            }).then((response) => response.json())
-            .then((data) => {
-                propertyID = data;
-            });  
-           
-        } catch (error) {
-            console.log("error in post property", error)
-        }
-
-
-
-        try {
-            
-            images.forEach(async (img) => {
-                formData.append("Id", String(0))
-                formData.append("propertyId", String(propertyID))
-                formData.append("image", img, img.name)
-                formData.append("uploadAt", String(format(new Date(), "yyyy/MM/dd h:m a")))
-                    
-                await fetch('https://localhost:7272/api/PropertiesImage', {
                 method: 'POST',
-                body: formData
-                }).then((response) => {
-                    if(response.status !== 200)
-                    {
-                        response.text().then(text => { throw new Error(text) })
-                    }
-                }) 
-            }); 
-            
-            navigate('/properties/add')
-        
+                headers: { 'Content-type': 'application/json; charset=UTF-8' },
+                body: JSON.stringify(propertyValue)
+
+            }).then((response) => response.json())
+                .then((data) => {
+                    propertyID = data;
+                });
+
         } catch (error) {
-            console.log("error in upload image", error)
+            console.log("error in post property", error);
+        }
+
+
+
+        try {
+
+            await fetch('https://localhost:7272/api/PropertiesAttribute', {
+                method: 'POST',
+                headers: { 'Content-type': 'application/json; charset=UTF-8' },
+                body: JSON.stringify(propertyValue)
+
+            }).then((response) => response.json())
+                .then((data) => {
+                    propertyID = data;
+                });
+
+        } catch (error) {
+            console.log("error in post property", error);
+        }
+
+
+
+        try {
+
+            images.forEach(async (img) => {
+                formData.append("Id", String(0));
+                formData.append("propertyId", String(propertyID));
+                formData.append("image", img, img.name);
+                formData.append("uploadAt", String(format(new Date(), "yyyy/MM/dd h:m a")));
+
+                await fetch('https://localhost:7272/api/PropertiesImage', {
+                    method: 'POST',
+                    body: formData
+                }).then((response) => {
+                    if (response.status !== 200) {
+                        response.text().then(text => { throw new Error(text); });
+                    }
+                });
+            });
+
+            navigate('/properties/add');
+
+        } catch (error) {
+            console.log("error in upload image", error);
             setUploadError(true);
         }
-    }
+    };
 
-    return(
+    return (
         <div>
 
             <div className="card m-5">
                 <h5 className="card-header">Agregar propiedad para vender/alquilar</h5>
                 <div className="card-body">
-            
-                    {uploadError ? 
-                     <div className="alert alert-dismissible alert-danger">
-                     <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
-                     Un error ocurrio en la subida de imagenes, verifique y trate de nuevo.
-                   </div>: ""}
-                    <form onSubmit={handleSubmit}> 
+
+                    {uploadError ?
+                        <div className="alert alert-dismissible alert-danger">
+                            <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
+                            Un error ocurrio en la subida de imagenes, verifique y trate de nuevo.
+                        </div> : ""}
+                    <form onSubmit={handleSubmit}>
 
                         <div className="form-group mb-3">
                             <input type="hidden" className="form-control" id="InputDate"
-                            name="createdAt" readOnly value={ format(new Date(), "yyyy/MM/dd h:m a")} onChange={handleChange}/>
+                                name="createdAt" readOnly value={format(new Date(), "yyyy/MM/dd h:m a")} onChange={handleChange} />
                             <span className="text-danger"></span>
                         </div>
-                        
+
                         <div className="form-group mb-3">
                             <label htmlFor="InputTitulo" className="form-label">Titulo</label>
                             <input type="text" className="form-control" id="InputTitulo"
-                            name="titulo" onChange={handleChange} />
+                                name="titulo" onChange={handleChange} />
                             <span className="text-danger"></span>
                         </div>
 
                         <div className="form-group mb-3">
                             <label htmlFor="InputPrecio" className="form-label">Precio</label>
                             <input type="number" className="form-control" id="InputPrecio"
-                            name="price" step="0.01" onChange={handleChange} />
+                                name="price" step="0.01" onChange={handleChange} />
                             <span className="text-danger"></span>
                         </div>
 
                         <div className="form-group mb-3">
                             <label htmlFor="InputPrecio" className="form-label">Area total (mts<sup>2</sup>)</label>
                             <input type="number" className="form-control" id="InputPrecio"
-                            name="areaTotal" step="0.01" onChange={handleChange} />
+                                name="areaTotal" step="0.01" onChange={handleChange} />
                             <span className="text-danger"></span>
                         </div>
 
-                        <AddAttributes attributes={attributes} propertyID={propertyID}/>
+                        <AddAttributes
+                            attributes={attributes}
+                            propertyID={propertyID}
+                            setAttributes={handleSetAttributes}
+                        />
 
                         <div className="form-group mb-4">
                             <h6>Imagenes a publicar</h6>
-                            <input type="file" name="file" onChange={handleImage}  accept="image/*" multiple={true} />
+                            <input type="file" name="file" onChange={handleImage} accept="image/*" multiple={true} />
                             <span className="text-danger"></span>
                         </div>
-            
-                        
+
+
                         <button type="submit" className="btn btn-primary me-2"><i className="bi bi-person-check me-1"></i>Guardar</button>
                     </form>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default AddProperty;
